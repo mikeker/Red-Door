c42aaba7f241899b8ecb243ed7ad61a75ee88091
Mike Keran <info@mikekeran.com>
2012-10-26 16:08:10 -0700

    temp checkin

---
+++ themes/red-door/plugins/layouts/reddoor_two_sidebar/reddoor-two-sidebar.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * Template for Red Door Single Sidebar
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ *     contentmain, sidebar1
+ */
+?>
+<div class="red-door-single-sidebar <?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div id="content" role="main">
+    <?php print $content['contentmain']; ?>
+  </div>
+  <aside class="sidebars">\
+    <div id="sidebar-1">
+      <?php print $content['sidebar1']; ?>
+    </div>
+    <div id="sidebar-2">
+      <?php print $content['sidebar2']; ?>
+    </div>
+  </aside>
+</div>
